e09e4eeda7e7abd4d740bf02d6396c3259b9001b
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-10-29 22:51:07 +0200

    done first exercise

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,15 @@
-const categories = document.querySelector("#categories");
-console.log(categories.children.length);
+const categories = document.getElementById("categories");
+console.log(`Number of categories: ${categories.children.length}`);
+
+// const arrayItem = [...categories.children];
+// arrayItem.forEach(function (element) {
+//   console.log(element);
+// });
+// console.log(arrayItem);
+
+const items = document.querySelectorAll(".item");
+items.forEach((item) => {
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
+  //   console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
