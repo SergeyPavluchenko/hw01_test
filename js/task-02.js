4ceff939f7d08d4f83693400187c61122b7e15a1
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-10-30 22:51:55 +0200

    second exercise

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredient = document.querySelector("#ingredients");
+console.log(ingredient);
+
+const arrIngredients = ingredients.map((ing) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ing;
+  return li;
+});
+
+ingredient.append(...arrIngredients);
