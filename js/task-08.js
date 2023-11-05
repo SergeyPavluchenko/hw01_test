b2e06706e1e257dc333d3fed4f1eb242429b00c0
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-11-05 17:55:27 +0200

    done exercise 9

--- js/task-08.js
+++ js/task-08.js
@@ -8,10 +8,10 @@ function onSubmit(e) {
   const { password } = e.currentTarget.elements;
   // const { length } = e.currentTarget.elements.email.value;
   e.preventDefault();
-  console.log("Email:", email.value.length);
-  console.log("Password:", password.value.length);
   console.log("Email:", email.value);
   console.log("Password:", password.value);
+  console.log("Email length:", email.value.length);
+  console.log("Password length:", password.value.length);
   if (email.value === "" || password.value === "") {
     alert("Заповніть всі поля");
   } else if (email.value.length < 10 || password.value.length < 10) {
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const box = document.querySelector(".widget");
+console.log(box);
+const color = document.querySelector(".color");
+console.log(color);
+const button = document.querySelector(".change-color");
+console.log(button);
+
+button.addEventListener("click", onClick);
+
+function onClick(e) {
+  const changeColor = getRandomHexColor();
+  // const changeColorBody = getRandomHexColor();
+  color.textContent = `${changeColor}`;
+  box.style.backgroundColor = changeColor;
+  // document.body.style.backgroundColor = changeColorBody;
+}
