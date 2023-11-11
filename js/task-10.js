bec1cf2916e2531e4fb995c8d0c527ab2445eec7
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-11-12 00:39:00 +0200

    finish 10 tast

--- js/task-10.js
+++ js/task-10.js
@@ -6,12 +6,14 @@
 
 // const input = document.querySelector("input");
 // const buttonCreate = document.querySelector("[data-create]");
-// const buttonDestroy = document.querySelector("[data-create]");
+// const buttonDestroy = document.querySelector("[data-destroy]");
 // const boxesEL = document.querySelector("#boxes");
 
 // let acc = [];
+// let num = 0;
 
 // buttonCreate.addEventListener("click", onClick);
+// buttonDestroy.addEventListener("click", destroyBoxes);
 
 // function onClick() {
 //   const numberValue = Number(input.value);
@@ -19,13 +21,19 @@
 //   for (let i = 1; i <= numberValue; i += 1) {
 //     const divEl = document.createElement("div");
 //     divEl.id = "item";
+//     divEl.textContent = num += 1;
 //     divEl.style.width = `${i * 10 + 20}px`;
 //     divEl.style.height = `${i * 10 + 20}px`;
-//     divEl.style.backgroundColor = color;
+//     divEl.style.background = color;
 //     acc.push(divEl);
 //   }
 //   boxesEL.append(...acc);
 //   console.log(boxesEL);
+// }
+
+// function destroyBoxes() {
+//   input.value = "";
+//   boxesEL.innerHTML = "";
 // }
 
 function getRandomHexColor() {
@@ -40,16 +48,25 @@ const buttonDestroy = document.querySelector("[data-create]");
 const boxesEL = document.querySelector("#boxes");
 
 let acc = [];
+let num = 0;
 
 buttonCreate.addEventListener("click", onClick);
 
 function onClick() {
   const numberValue = Number(input.value);
+  const box = created(numberValue);
+  console.log(box);
+  return box;
+}
+
+function created(amount) {
   const color = getRandomHexColor();
-  for (let i = 1; i <= numberValue; i += 1) {
-    const el = `<div class='item></div>`;
+
+  for (let i = 1; i <= amount; i += 1) {
+    const el = `<div class='item-div'>${(num += 1)}</div>`;
     acc.push(el);
   }
   boxesEL.insertAdjacentHTML("beforeend", acc);
   console.log(boxesEL);
+  return boxesEL;
 }
